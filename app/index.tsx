00f4c2b3d15d9142acb8a7ea48f24d48fcc18765
GabrielPlus <gabrielpius0311@gmail.com>
2024-12-30 15:19:24 +0000

    onboarding

---
+++ app/index.tsx
@@ -0,0 +1,15 @@
+import { View, Text } from 'react-native'
+import React, { useEffect, useState } from 'react'
+
+export default function index() {
+    const [loggedInUser, setLoggedInUser] = useState(true);
+    const [loading, setloading] = useState(true);
+
+    useEffect
+
+  return (
+    <View>
+      <Text>index</Text>
+    </View>
+  )
+}
