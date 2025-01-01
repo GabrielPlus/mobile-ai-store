855081d2b52bd0358d7e5a43f876fa5286b27bc6
GabrielPlus <gabrielpius0311@gmail.com>
2025-01-01 14:29:04 +0000

    tabs...onboarding tab

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,27 @@
-import { View, Text } from 'react-native'
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from "react";
+import * as SecureStore from "expo-secure-store";
+import { Redirect } from "expo-router";
 
 export default function index() {
-    const [loggedInUser, setLoggedInUser] = useState(true);
-    const [loading, setloading] = useState(true);
+  const [loggedInUser, setloggedInUser] = useState(false);
+  const [loading, setLoading] = useState(true);
 
-    useEffect
+  useEffect(() => {
+    const subscription = async () => {
+      const token = SecureStore.getItem("accessToken");
+      setloggedInUser(token ? true : false);
+      setLoading(false);
+    };
+    subscription();
+  }, []);
 
   return (
-    <View>
-      <Text>index</Text>
-    </View>
-  )
+    <>
+      {loading ? (
+        <></>
+      ) : (
+        <Redirect href={!loggedInUser ? "/(routes)/onboarding" : "/(tabs)"} />
+      )}
+    </>
+  );
 }
